26a6def94866d6142e3b88e21ca24837afceb334
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-06-13 00:00:47 +0600

    Update wedevs-academy.php

--- wedevs-academy.php
+++ wedevs-academy.php
@@ -3,7 +3,7 @@
  * Plugin Name:       weDevs Academy
  * Description:       https://www.youtube.com/watch?v=D_I3qpuGKno&list=PLx7dNwJLCzHldCT_F1uOBELcSYktvrePO
  * Version:           1.0
- * Author:            Sohan
+ * Author:            Sohan 12 mins
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages
  */
@@ -12,47 +12,58 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-
+// The main plugin
 final class WeDevs_Academy {
 
-	// class constructor
-	function __construct() {
+	/**
+	 * Plugin version
+	 * 
+	 * @var string
+	 */
+	const VERSION = '1.0';
+
+	// Class constructor
+	private function __construct() {
+		$this->define_constants();
+
 
 	}
-
-	/**
-	 * 
-	 * 
-	 * @return
-	*/
-
-
 
 	/**
 	 * Initializes a Singleton instance
 	 * 
 	 * @return \WeDevs_Academy
-	*/
+	 */
 	public static function init() {
 		static $instance = false; 
 
 		if ( ! $instance ) {
 			$instance = new self();
-		}  
+		} 
+		
+		return $instance;
 	}
 
 	/**
-	 * Initializes the main plugin
-	 * 
-	 * @return \WeDevs_Academy
-	*/
-	function wedevs_academy() {
-		return WeDevs_Academy()::init();
+	 * Define the constants
+	 */
+	public function define_constants() {
+		define( 'WD_ACADEMY_VERSION', self::VERSION );
+		define( 'WD_ACADEMY_FILE', __FILE__ );
+		define( 'WD_ACADEMY_PATH', __DIR__ );
+		define( 'WD_ACADEMY_URL', plugins_url( '', WD_ACADEMY_FILE ) );
+		define( 'WD_ACADEMY_ASSETS', WD_ACADEMY_URL . '/assets' );
 	}
-
 }
 
+/**
+ * Initializes the main plugin
+ * 
+ * @return \WeDevs_Academy
+ */
+function wedevs_academy() {
+	return WeDevs_Academy::init();
+}
 
-
-
-
+// Initialize the plugin
+wedevs_academy();
