d71f89d2471d4de31eae7689fb56ef61462830a6
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-06-12 21:55:23 +0600

    Initial commit

---
+++ wedevs-academy.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Plugin Name:       weDevs Academy
+ * Description:       https://www.youtube.com/watch?v=D_I3qpuGKno&list=PLx7dNwJLCzHldCT_F1uOBELcSYktvrePO
+ * Version:           1.0
+ * Author:            Sohan
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+/**
+ * The main plugin class
+ * */
+
+
+
+
+
+
+
+
