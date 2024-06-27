47986961511dc8cebf2b932db2f26855e37c161d
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-06-27 23:58:40 +0600

    Update Installer.php

--- includes/Installer.php
+++ includes/Installer.php
@@ -22,7 +22,15 @@ class Installer {
     public function create_tables(){
         global $wpdb;
 
-        $schema = "CREATE TABLE IF NOT EXISTS } `` "
+        $schema = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ac_addresses` (
+            `id` int(11) unsigned NOT NULL AUTO_INCREMENT, 
+            `name` varchar(100) NOT NULL,
+            `address` varchar(255) DEFAULT NULL,
+            `phone` varchar(30) DEFAULT NULL,
+            `created_by` bigint(20) unsigned NOT NULL,
+            `created_at` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
     }
 
