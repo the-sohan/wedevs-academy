bf9dd04961a8938fbdd3e8adb03831328b648095
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-07-09 05:59:14 +0600

    Class4-15.44

--- includes/Admin/Address_List.php
+++ includes/Admin/Address_List.php
@@ -26,6 +26,10 @@ class Address_List extends \WP_List_Table {
     }
 
     public function prepare_items() {
-        
+        $columns = $this->get_columns();
+        $hidden = [];
+        $sortable = $this->get_sortable_columns();
+
+        $this->_column_headers = [ $columns, $hidden, $sortable ];
     }
 }
--- includes/Admin/views/address-list.php
+++ includes/Admin/views/address-list.php
@@ -1,4 +1,15 @@
 <div class="wrap">
+
     <h1 class="wp-heading-inline"><?php _e( 'Address book', 'wedevs-academy' ); ?></h1>
+    
     <a href="<?php echo admin_url( 'admin.php?page=wedevs-academy&action=new' ); ?>" class="page-title-action">Add New</a>
+
+    <form action="" method="post">
+        <?php
+        $table = new WeDevs\Academy\Admin\Address_List();
+        $table->prepare_items();
+        $table->display();
+        ?>
+    </form>
+
 </div>
