95e7b55b5f2bf2392c7f3571d49e32abcdb58d15
Sohan Chowdhury <pro.sohan7@gmail.com>
2024-09-11 22:37:54 +0600

    Update wedevs-academy.php

--- wedevs-academy.php
+++ wedevs-academy.php
@@ -3,7 +3,7 @@
  * Plugin Name:       weDevs Academy
  * Description:       https://www.youtube.com/watch?v=D_I3qpuGKno&list=PLx7dNwJLCzHldCT_F1uOBELcSYktvrePO
  * Version:           1.0 
- * Author:            Sohan - class 7 - 18.00 mins
+ * Author:            Sohan 
  * Text Domain:       wedevs-academy
  * Domain Path:       /languages
  */
